cd6d3615baeb96dd3668cfada9f100b427449cf2
Paulo Stein <pauloeduardostein@gmail.com>
2020-12-10 01:04:58 -0300

    feat: testing routes, controllers and views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Currency;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('currency', [Currency::class, 'currency'])->name('currency');
+
+Route::resource('projects', Currency::class);
