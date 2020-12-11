8599fd75b6ce02edf9d805d2fed6f9a2219718d9
Paulo Stein <pauloeduardostein@gmail.com>
2020-12-11 19:38:19 -0300

    feat: improve charts and add filter

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -9,8 +9,8 @@ class CurrencyController extends Controller
     public function currency()
     {
         $client = new Client(); //GuzzleHttp\Client
+        
         $url = "https://economia.awesomeapi.com.br/json/all";
-
 
         $response = $client->request('GET', $url, [
             'verify'  => false,
@@ -25,13 +25,20 @@ class CurrencyController extends Controller
     {
         $client = new Client(); //GuzzleHttp\Client
         $codeCurrency = htmlspecialchars($_POST['currency']);
-        $url = "https://economia.awesomeapi.com.br/json/daily/" . $codeCurrency . "-BRL/5";
+        $daysChart = htmlspecialchars($_POST['days']);
+
+        $url = "https://economia.awesomeapi.com.br/json/daily/" . $codeCurrency . "-BRL/" . $daysChart;
+        $url2 = "https://economia.awesomeapi.com.br/json/all";
+        $response2 = $client->request('GET', $url2, [
+            'verify'  => false,
+        ]);
+        $responseBody2 = json_decode($response2->getBody());
 
         $response = $client->request('GET', $url, [
             'verify'  => false,
         ]);
 
         $responseBody = json_decode($response->getBody());
-        return view('currency-code', compact('responseBody', 'codeCurrency'));
+        return view('currency-code', compact('responseBody', 'codeCurrency','responseBody2'));
     }
 }
